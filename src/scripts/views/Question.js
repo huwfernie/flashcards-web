89b2061a91779cff1e43bd9a20acc54e7ecd63c3
huwfernie <huwfernie@gmail.com>
2023-12-11 11:24:29 +0000

    refactor Question component

--- src/scripts/views/Question.js
+++ src/scripts/views/Question.js
@@ -4,35 +4,40 @@ import Footer from '../components/Footer.js';
 import TitleBar from '../components/Header.js';
 
 function Question({ data, store, answer = false }) {
-  let _className, _title, _text;
-  
-  const question = data[store.deckIndex].chapters[store.chapterIndex].questions[store.questionIndex];
-  
-  if (answer === false) {
-    const name = store.checkView("question");
-    _className = "view view-question " + name;
-    _title = "Question";
-    _text = question.questionText;
-  } else {
-    const name = store.checkView("question");
-    _className = "view view-answer " + name;
-    _title = "Answer";
-    _text = question.answerText;
-  }
+  let _className = "view view-";
+  let _title, _text;
 
-  _text = _text.replace(/\*\*(.*?)\*\*/g , "<strong>$1</strong>");
-  _text = _text.replace(/\*(.*?)\*/g , "<em>$1</em>");
+  try {
+    const question = data[store.deckIndex].chapters[store.chapterIndex].questions[store.questionIndex];
 
-  return (
-    <div className={_className}>
-      <TitleBar title={_title} store={store} version="backMenu" />
-      <div className="content">
-        <p className="question-text" dangerouslySetInnerHTML={{__html: _text}}></p>
-        { question.externalLink !== undefined && answer === true ? <p><a className="external-link" href={question.externalLink}>Learn More</a></p> : null} 
+    if (answer === false) {
+      const name = store.checkView("question");
+      _className += "question " + name;
+      _title = "Question";
+      _text = question.questionText;
+    } else {
+      const name = store.checkView("question");
+      _className += "answer " + name;
+      _title = "Answer";
+      _text = question.answerText;
+    }
+
+    _text = _text.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
+    _text = _text.replace(/\*(.*?)\*/g, "<em>$1</em>");
+
+    return (
+      <div className={_className}>
+        <TitleBar title={_title} store={store} version="backMenu" />
+        <div className="content">
+          <p className="question-text" dangerouslySetInnerHTML={{ __html: _text }}></p>
+          {question.externalLink !== undefined && answer === true ? <p><a className="external-link" href={question.externalLink}>Learn More</a></p> : null}
+        </div>
+        <Footer store={store} version="question" />
       </div>
-      <Footer store={store} version="question" />
-    </div>
-  );
+    );
+  } catch (error) {
+    return <div><p>Something went wrong.</p></div>
+  }
 }
 
 export default Question;
