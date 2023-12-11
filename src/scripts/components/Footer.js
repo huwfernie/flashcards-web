a55bc8503ae6796b2a632afaa035bb8fb736b12e
huwfernie <huwfernie@gmail.com>
2023-12-11 12:33:43 +0000

    Add css font size media querys

--- src/scripts/components/Footer.js
+++ src/scripts/components/Footer.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
-function Footer({store, version, toggleAnswer}) {
+function Footer({ store, version, toggleAnswer }) {
 
     if (version === "question") {
         return (
             <footer className="footer">
-                <span onClick={store.prevQuestion}>Prev</span>
-                <span onClick={store.toggleAnswer}>Flip</span>
-                <span onClick={store.nextQuestion}>Next</span>
+                <span onClick={store.prevQuestion}><p>Prev</p></span>
+                <span onClick={store.toggleAnswer}><p>Flip</p></span>
+                <span onClick={store.nextQuestion}><p>Next</p></span>
             </footer>
         )
     }
     else {
         return (
             <footer className="footer">
-                FlashCards
+                <p>
+                    FlashCards
+                </p>
             </footer>
         )
     }
